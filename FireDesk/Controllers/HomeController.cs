1f444877362a658fccca5806aad41cc8cbebdbbb
Gabriel Matos <gabrieldevbrasilia@gmail.com>
2023-11-27 10:53:23 -0300

    commit inicial

---
+++ FireDesk/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using FireDesk.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using FireDesk.DAO;
+using Microsoft.Extensions.Configuration;
+
+namespace FireDesk.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FireDesk/DAO/Dashboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace FireDesk.DAO
+{
+    public class Dashboard
+    {
+        private readonly IConfiguration _configuration;
+
+        public Dashboard(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
