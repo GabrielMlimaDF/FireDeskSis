a19e3e9cd4ee70d3575fb9b561395373009c7b70
Gabriel Matos <gabrieldevbrasilia@gmail.com>
2023-12-15 17:35:01 -0300

    Finalização do frontend do formulário de chamados

--- FireDesk/Models/TicketsModel.cs
+++ FireDesk/Models/TicketsModel.cs
@@ -1,12 +1,31 @@
-﻿namespace FireDesk.Models
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace FireDesk.Models
 {
     public class TicketsModel
     {
+        [Required]
         public int TicketID { get; set; }
+
+        [Required]
         public string? Assunto { get; set; }
+
+        [Required]
         public string? Descricao { get; set; }
+
         public string? Status { get; set; }
+
+        [Required]
         public string? Tecnico { get; set; }
-        public string? UsuarioAbertura { get; set; }
+
+        public string? UsuarioAbertura { get; set; } = null;
+        public string? UsuarioFechamento { get; set; } = null;
+
+        [Required]
+        public DateTime DataAbertura { get; set; }
+
+        [Required]
+        public string? Prioridade { get; set; }
     }
 }
---
+++ FireDesk/wwwroot/js/Actions/FormChamados.js
@@ -0,0 +1,13 @@
+﻿// Função para mostrar o modal e a sobreposição
+function mostrarModal() {
+    document.getElementById("modalOverlay").style.display = "block";
+    var modal = document.getElementById("modal");
+    modal.style.right = '0';
+}
+
+// Função para ocultar o modal e a sobreposição
+function fecharModal() {
+    document.getElementById("modalOverlay").style.display = "none";
+    var modal = document.getElementById("modal");
+    modal.style.right = '-100%';
+}
