ca4749b2a86fa4e8d3664aef20a5766cf82d8881
Gabriel Matos <gabrieldevbrasilia@gmail.com>
2024-01-17 16:24:49 -0300

    Css do select da lista de técnicos FormChamados

--- FireDesk/wwwroot/js/Actions/FormChamados.js
+++ FireDesk/wwwroot/js/Actions/FormChamados.js
@@ -11,3 +11,19 @@ function fecharModal() {
     var modal = document.getElementById("modal");
     modal.style.right = '-100%';
 }
+//Função para o select
+let select = document.querySelector('.select2'),
+    selectedValue = document.getElementById('selected-value'),
+    optionsViewButton = document.getElementById('options-view-button'),
+    inputsOptions = document.querySelectorAll('.option input')
+
+inputsOptions.forEach(input => {
+    input.addEventListener('click', event => {
+        selectedValue.textContent = input.dataset.label
+
+        const isMouseOrTouch =
+            event.pointerType == "mouse" ||
+            event.pointerType == "touch"
+        isMouseOrTouch && optionsViewButton.click()
+    })
+})
